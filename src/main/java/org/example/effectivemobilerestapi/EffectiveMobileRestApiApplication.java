e24b822be1fff14a2c24f1638de8ccbfaa1e493b
Spakir <danntyl@mail.ru>
2025-03-25 18:50:27 +0300

    Начальная конфигурация проекта

---
+++ src/main/java/org/example/effectivemobilerestapi/EffectiveMobileRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.effectivemobilerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveMobileRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveMobileRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/effectivemobilerestapi/EffectiveMobileRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.effectivemobilerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveMobileRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
