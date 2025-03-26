92c1281940b9b74b18bcb158940ce4d46f3b5021
Spakir <danntyl@mail.ru>
2025-03-26 23:46:23 +0300

    Dto комментария к задачам

---
+++ src/main/java/org/example/effectivemobilerestapi/dto/CommentDto.java
@@ -0,0 +1,24 @@
+package org.example.effectivemobilerestapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class CommentDto {
+
+    private Long id;
+
+    @NotBlank(message = "Комментарий не может быть null")
+    @Size(max = 200, message = "Комментарий не может быть длиннее 200 символов")
+    private String text;
+
+    private String author;
+
+    @NotBlank(message = "Id задачи не может быть null")
+    private Long taskId;
+
+    private LocalDateTime createdAt;
+}
