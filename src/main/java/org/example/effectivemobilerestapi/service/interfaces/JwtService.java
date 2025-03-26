927db74ea4eba8036a9cb81110ea419122f5f9f2
Spakir <danntyl@mail.ru>
2025-03-26 05:24:49 +0300

    Изменение контракта JwtService

--- src/main/java/org/example/effectivemobilerestapi/service/interfaces/JwtService.java
+++ src/main/java/org/example/effectivemobilerestapi/service/interfaces/JwtService.java
@@ -1,13 +1,11 @@
-package org.example.effectivemobilerestapi.service;
+package org.example.effectivemobilerestapi.service.interfaces;
 
 import io.jsonwebtoken.Claims;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 import java.util.function.Function;
-
 
 public interface JwtService {
 
@@ -15,13 +13,13 @@ public interface JwtService {
 
     <T> T extractClaim(String token, Function<Claims, T> claimsReceiver);
 
-    String generateToken(UserDetails userDetails);
+    String generateToken(String username);
 
-    String generateToken(Map<String, Object> claims, UserDetails userDetails);
+    String generateToken(Map<String, Object> claims, String username);
 
-    boolean isTokenValid(String token, UserDetails userDetails);
+    boolean isTokenValid(String token, String username);
 
-    String buildToken(Map<String, Object> claims, UserDetails userDetails, long expireTime);
+    String buildToken(Map<String, Object> claims, String username, long expireTime);
 
     boolean isTokenExpired(String token);
 
