5c67803240916346184091e839a9fd2d5ecadb6d
Spakir <danntyl@mail.ru>
2025-03-26 05:21:37 +0300

    Репозиторий для работы с таблицей Users

---
+++ src/main/java/org/example/effectivemobilerestapi/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package org.example.effectivemobilerestapi.repository;
+
+import org.example.effectivemobilerestapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
