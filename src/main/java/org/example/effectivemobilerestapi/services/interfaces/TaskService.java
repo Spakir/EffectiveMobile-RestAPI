79eda13188890bd46926ff61f6832bb8d3db32d3
Spakir <danntyl@mail.ru>
2025-03-27 02:59:12 +0300

    Добавление метода getTaskById

--- src/main/java/org/example/effectivemobilerestapi/services/interfaces/TaskService.java
+++ src/main/java/org/example/effectivemobilerestapi/services/interfaces/TaskService.java
@@ -1,23 +1,25 @@
 package org.example.effectivemobilerestapi.services.interfaces;
 
 import org.example.effectivemobilerestapi.dto.TaskDto;
+import org.example.effectivemobilerestapi.entities.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.nio.file.AccessDeniedException;
+import java.util.Optional;
+
 public interface TaskService {
 
-    TaskDto saveTask(UserDetails user,TaskDto newTaskDto);
+    TaskDto saveTask(UserDetails user, TaskDto newTaskDto);
 
-    TaskDto updateTask(UserDetails user,TaskDto updatedTaskDto);
+    TaskDto updateTask(UserDetails user, Long id, TaskDto updatedTaskDto) throws AccessDeniedException;
 
-    void deleteTask(Long id, UserDetails user);
+    void deleteTask(Long id, UserDetails user) throws AccessDeniedException;
+
+    TaskDto getTaskById(Long id);
 
     Page<TaskDto> getTasksByAuthor(UserDetails user, Pageable pageable);
 
     Page<TaskDto> getTasksByExecutor(UserDetails user, Pageable pageable);
-
-    TaskDto changeExecutor(Long taskId, String executorEmail, UserDetails user);
-
-    TaskDto changeTaskStatus(Long taskId, String status, UserDetails user);
 }
