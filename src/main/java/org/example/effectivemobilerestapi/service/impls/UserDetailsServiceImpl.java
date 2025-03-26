58865f147e28b9115c37beb4e1c6161bd2a143ca
Spakir <danntyl@mail.ru>
2025-03-26 05:23:39 +0300

    Реализация интерфейса UserDetailsService

---
+++ src/main/java/org/example/effectivemobilerestapi/service/impls/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package org.example.effectivemobilerestapi.service.impls;
+
+import lombok.RequiredArgsConstructor;
+import org.example.effectivemobilerestapi.entity.User;
+import org.example.effectivemobilerestapi.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow();
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole().name()))
+        );
+    }
+}
