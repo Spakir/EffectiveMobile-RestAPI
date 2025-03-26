3447dec4efdce3657a5224017d85bf923a483734
Spakir <danntyl@mail.ru>
2025-03-26 05:25:32 +0300

    Изменение параметра метода jwtService.isTokenValid()

--- src/main/java/org/example/effectivemobilerestapi/filter/JwtAuthFilter.java
+++ src/main/java/org/example/effectivemobilerestapi/filter/JwtAuthFilter.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.example.effectivemobilerestapi.service.JwtService;
+import org.example.effectivemobilerestapi.service.interfaces.JwtService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -41,7 +41,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
 
-            if (jwtService.isTokenValid(jwt, userDetails)){
+            if (jwtService.isTokenValid(jwt, userDetails.getUsername())){
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
