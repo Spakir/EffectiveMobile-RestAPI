9e1926e0ee09129b96efd1d2ef9345e4cbd4bc14
Spakir <danntyl@mail.ru>
2025-03-27 21:41:26 +0300

    Описание API с помощью SWAGGER

--- src/main/java/org/example/effectivemobilerestapi/controllers/TaskController.java
+++ src/main/java/org/example/effectivemobilerestapi/controllers/TaskController.java
@@ -1,7 +1,13 @@
 package org.example.effectivemobilerestapi.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.effectivemobilerestapi.dto.ErrorResponseDto;
 import org.example.effectivemobilerestapi.dto.TaskDto;
 import org.example.effectivemobilerestapi.services.interfaces.TaskService;
 import org.springframework.data.domain.Page;
@@ -17,22 +23,69 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/task")
 @Validated
+@Tag(name = "Task API")
 public class TaskController {
 
     private final TaskService taskService;
 
+    @Operation(summary = "Создание новой задачи")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Задача успешно создана",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = TaskDto.class))
+    )
+    @ApiResponse(
+            responseCode = "500",
+            description = "Ошибка с валидацией входных данных",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
+    @ApiResponse(
+            responseCode = "403",
+            description = "Ошибка авторизации",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
     @PostMapping("/new")
     public TaskDto saveTask(@AuthenticationPrincipal UserDetails user, @Valid @RequestBody TaskDto taskDto) {
         return taskService.saveTask(user, taskDto);
     }
 
+    @Operation(summary = "Обновление задачи")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Задача успешно обновлена",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = TaskDto.class))
+    )
+    @ApiResponse(
+            responseCode = "500",
+            description = "Ошибка с валидацией входных данных",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
+    @ApiResponse(
+            responseCode = "403",
+            description = "Недостаточно прав для действия",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
     @PutMapping("/{id}")
     public TaskDto updateTaskDto(@AuthenticationPrincipal UserDetails user,
                                  @PathVariable("id") Long id,
                                  @Valid @RequestBody TaskDto taskDto) {
+
         return taskService.updateTask(user, id, taskDto);
     }
 
+    @Operation(summary = "Получение всех задач, созданных пользователем, который сделал запрос")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Получение всех задач",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = TaskDto.class))
+    )
     @GetMapping("/by-author")
     public Page<TaskDto> getTasksByAuthor(@AuthenticationPrincipal UserDetails user,
                                           @RequestParam(defaultValue = "0") int page,
@@ -45,6 +98,13 @@ public class TaskController {
         return taskService.getTasksByAuthor(user, pageable);
     }
 
+    @Operation(summary = "Получение всех задач, для которых пользователь является исполнителем")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Успешное получение всех задач",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = TaskDto.class))
+    )
     @GetMapping("/by-executor")
     public Page<TaskDto> getTasksByExecutor(@AuthenticationPrincipal UserDetails user,
                                             @RequestParam(defaultValue = "0") int page,
@@ -57,6 +117,24 @@ public class TaskController {
         return taskService.getTasksByExecutor(user, pageable);
     }
 
+    @Operation(summary = "Удаление задачи по ID")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Успешное удаление задачи",
+            content = @Content(schema = @Schema(implementation = Void.class))
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Задача с данным ID не была найдена",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
+    @ApiResponse(
+            responseCode = "403",
+            description = "У пользователя нет прав для данного действия",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
     @DeleteMapping("/{id}")
     public void deleteTask(@AuthenticationPrincipal UserDetails user,
                            @PathVariable("id") Long id) {
@@ -64,6 +142,25 @@ public class TaskController {
         taskService.deleteTask(id, user);
     }
 
+    @Operation(summary = "Получение задачи по ID")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Задача успешно найдена",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Задача с данным ID не была найдена",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
+    @ApiResponse(
+            responseCode = "403",
+            description = "У пользователя нет прав для данного действия",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
     @GetMapping("/{id}")
     public TaskDto getTaskById(@AuthenticationPrincipal UserDetails user,
                                @PathVariable("id") Long id) {
@@ -71,6 +168,31 @@ public class TaskController {
         return taskService.getTaskById(id);
     }
 
+    @Operation(summary = "Обновление исполнителя задачи")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Исполнитель задачи успешно обновлён",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = TaskDto.class))
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Задача с данным ID не была найдена",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
+    @ApiResponse(
+            responseCode = "403",
+            description = "У пользователя нет прав для данного действия",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
+    @ApiResponse(
+            responseCode = "500",
+            description = "Ошибка валидации данных",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponseDto.class))
+    )
     @PutMapping("update-executor-{id}")
     public TaskDto changeExecutor(@AuthenticationPrincipal UserDetails user,
                                   @PathVariable("id") Long id,
