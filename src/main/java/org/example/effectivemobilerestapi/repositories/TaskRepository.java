df6bc2366844d1e1ff909345f49c3c98743b6c5a
Spakir <danntyl@mail.ru>
2025-03-27 16:53:55 +0300

    Добавление @EntityGraph для решения проблемы N + 1

--- src/main/java/org/example/effectivemobilerestapi/repositories/TaskRepository.java
+++ src/main/java/org/example/effectivemobilerestapi/repositories/TaskRepository.java
@@ -3,6 +3,7 @@ package org.example.effectivemobilerestapi.repositories;
 import org.example.effectivemobilerestapi.entities.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +11,11 @@ import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @EntityGraph(attributePaths = {"comments", "author", "executor"})
     Page<Task> findByAuthorEmail(String author, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"comments", "author", "executor"})
     Page<Task> findByExecutorEmail(String executor, Pageable pageable);
 
     Optional<Task> findById(Long id);
