9426b773373f32427e0a24527c11b2620ff2b92a
Spakir <danntyl@mail.ru>
2025-03-27 16:50:32 +0300

    Обновление сущности Comment

--- src/main/java/org/example/effectivemobilerestapi/entities/Comment.java
+++ src/main/java/org/example/effectivemobilerestapi/entities/Comment.java
@@ -1,10 +1,7 @@
 package org.example.effectivemobilerestapi.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Comment {
 
     @Id
@@ -27,11 +25,11 @@ public class Comment {
     private LocalDateTime createdAt;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "task_id", nullable = false)
+    @JoinColumn(name = "task_id")
     private Task task;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "author_id")
     private User author;
 
     @PrePersist
