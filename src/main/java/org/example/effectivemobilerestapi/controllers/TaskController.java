24c9273d1cd20e8fe61a69e225b25d5fcae55ac3
Spakir <danntyl@mail.ru>
2025-03-27 03:07:56 +0300

    Контроллер для работы с Tasks

---
+++ src/main/java/org/example/effectivemobilerestapi/controllers/TaskController.java
@@ -0,0 +1,73 @@
+package org.example.effectivemobilerestapi.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.effectivemobilerestapi.dto.TaskDto;
+import org.example.effectivemobilerestapi.services.interfaces.TaskService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @PostMapping("/new")
+    public TaskDto saveTask(@AuthenticationPrincipal UserDetails user, @Valid @RequestBody TaskDto taskDto) {
+        return taskService.saveTask(user, taskDto);
+    }
+
+    @PutMapping("/{id}")
+    public TaskDto updateTaskDto(@AuthenticationPrincipal UserDetails user,
+                                 @PathVariable("id") Long id,
+                                 @Valid @RequestBody TaskDto taskDto) throws AccessDeniedException {
+        return taskService.updateTask(user, id, taskDto);
+    }
+
+    @GetMapping("/by-author")
+    public Page<TaskDto> getTasksByAuthor(@AuthenticationPrincipal UserDetails user,
+                                          @RequestParam(defaultValue = "0") int page,
+                                          @RequestParam(defaultValue = "10") int size,
+                                          @RequestParam(defaultValue = "priority") String sort,
+                                          @RequestParam(defaultValue = "desc") String direction) {
+        Sort.Direction sortDirection = Sort.Direction.fromString(direction);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sort));
+
+        return taskService.getTasksByAuthor(user, pageable);
+    }
+
+    @GetMapping("/by-executor")
+    public Page<TaskDto> getTasksByExecutor(@AuthenticationPrincipal UserDetails user,
+                                            @RequestParam(defaultValue = "0") int page,
+                                            @RequestParam(defaultValue = "10") int size,
+                                            @RequestParam(defaultValue = "priority") String sort,
+                                            @RequestParam(defaultValue = "desc") String direction) {
+        Sort.Direction sortDirection = Sort.Direction.fromString(direction);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sort));
+
+        return taskService.getTasksByExecutor(user, pageable);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@AuthenticationPrincipal UserDetails user,
+                           @PathVariable("id") Long id) throws AccessDeniedException {
+
+        taskService.deleteTask(id, user);
+    }
+
+    @GetMapping("/{id}")
+    public TaskDto getTaskById(@AuthenticationPrincipal UserDetails user,
+                            @PathVariable("id") Long id){
+
+        return taskService.getTaskById(id);
+    }
+}
