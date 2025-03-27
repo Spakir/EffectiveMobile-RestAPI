d07f7303baf6a9a99e32dd1114a3cb2965ed23a0
Spakir <danntyl@mail.ru>
2025-03-27 03:00:38 +0300

    Добавление аннотации @Data

--- src/main/java/org/example/effectivemobilerestapi/dto/UserDto.java
+++ src/main/java/org/example/effectivemobilerestapi/dto/UserDto.java
@@ -3,28 +3,27 @@ package org.example.effectivemobilerestapi.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.example.effectivemobilerestapi.enums.Role;
 
-@AllArgsConstructor
-public class UserDto {
+import java.util.List;
 
+@AllArgsConstructor
+@Data
+public class UserDto {
     private Long id;
 
     @NotBlank(message = "Почта не может быть пустой")
     @Size(max = 255, message = "Длина почты не может превышать 255 символов")
-    @Setter
-    @Getter
     private String email;
 
     @NotBlank(message = "Пароль не может быть пустым")
     @Size(max = 200, message = "Длина пароля не может превышать 200 символов")
-    @Setter
-    @Getter
     private String password;
 
-    @Setter
-    @Getter
     private Role role;
+
+    private List<TaskDto> createdTasks;
+
+    private List<TaskDto> assignedTasks;
 }
