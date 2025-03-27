356da7cf28bd67e569991f7c03df9cb0c6371220
Spakir <danntyl@mail.ru>
2025-03-27 03:01:15 +0300

    Добавление метода mapTaskToDto

--- src/main/java/org/example/effectivemobilerestapi/mappers/UserMapper.java
+++ src/main/java/org/example/effectivemobilerestapi/mappers/UserMapper.java
@@ -1,35 +1,48 @@
 package org.example.effectivemobilerestapi.mappers;
 
-import org.example.effectivemobilerestapi.enums.Role;
+import org.example.effectivemobilerestapi.dto.TaskDto;
 import org.example.effectivemobilerestapi.dto.UserDto;
+import org.example.effectivemobilerestapi.entities.Task;
 import org.example.effectivemobilerestapi.entities.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.mapstruct.*;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = TaskMapper.class)
 public interface UserMapper {
 
-    User toUser(UserDto userDto);
-
+    @Mapping(target = "createdTasks", source = "createdTasks", qualifiedByName = "mapTasksToDto")
+    @Mapping(target = "assignedTasks", source = "assignedTasks", qualifiedByName = "mapTasksToDto")
     UserDto toUserDto(User user);
 
-    @Mapping(target = "username", source = "email")
-    @Mapping(target = "authorities", expression = "java(mapAuthority(userDto.getRole()))")
+    @Mapping(target = "createdTasks", ignore = true)
+    @Mapping(target = "assignedTasks", ignore = true)
+    User toUser(UserDto userDto);
+
+    @Named("mapTasksToDto")
+    default List<TaskDto> mapTasksToDto(List<Task> tasks) {
+        if (tasks == null || tasks.isEmpty()) {
+            return null;
+        }
+        return tasks.stream()
+                .map(task -> {
+                    TaskDto dto = new TaskDto();
+                    dto.setId(task.getId());
+                    dto.setHeader(task.getHeader());
+                    dto.setDescription(task.getDescription());
+
+                    return dto;
+                })
+                .collect(Collectors.toList());
+    }
+
     default UserDetails toUserDetails(UserDto userDto) {
         return org.springframework.security.core.userdetails.User
                 .withUsername(userDto.getEmail())
                 .password(userDto.getPassword())
                 .authorities(userDto.getRole().name())
                 .build();
-    }
-
-    default Collection<? extends GrantedAuthority> mapAuthority(Role role) {
-        return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
     }
 }
