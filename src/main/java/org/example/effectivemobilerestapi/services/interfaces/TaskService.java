4a542e109d63d4d5fe51a39b1bca65a7d527709c
Spakir <danntyl@mail.ru>
2025-03-27 16:55:07 +0300

    Добавление методов: changeExecutor

--- src/main/java/org/example/effectivemobilerestapi/services/interfaces/TaskService.java
+++ src/main/java/org/example/effectivemobilerestapi/services/interfaces/TaskService.java
@@ -6,20 +6,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.nio.file.AccessDeniedException;
-import java.util.Optional;
-
 public interface TaskService {
 
     TaskDto saveTask(UserDetails user, TaskDto newTaskDto);
 
-    TaskDto updateTask(UserDetails user, Long id, TaskDto updatedTaskDto) throws AccessDeniedException;
+    TaskDto updateTask(UserDetails user, Long id, TaskDto updatedTaskDto);
 
-    void deleteTask(Long id, UserDetails user) throws AccessDeniedException;
+    Task getTaskEntityById(Long id);
+
+    void deleteTask(Long id, UserDetails user);
 
     TaskDto getTaskById(Long id);
 
     Page<TaskDto> getTasksByAuthor(UserDetails user, Pageable pageable);
 
     Page<TaskDto> getTasksByExecutor(UserDetails user, Pageable pageable);
+
+    TaskDto changeExecutor(UserDetails user, Long taskId, String executorName);
 }
