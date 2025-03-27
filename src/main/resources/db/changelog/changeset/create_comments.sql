8ddcd1092e3c0a23c875da0fef19425f4f0975e8
Spakir <danntyl@mail.ru>
2025-03-27 16:49:46 +0300

    Обновленный запрос на создание таблицы Comments

--- src/main/resources/db/changelog/changeset/create_comments.sql
+++ src/main/resources/db/changelog/changeset/create_comments.sql
@@ -1,12 +1,11 @@
-CREATE TABLE IF NOT EXISTS Comments (
+CREATE TABLE IF NOT EXISTS Comments
+(
     id BIGSERIAL PRIMARY KEY,
     text VARCHAR(200) NOT NULL,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     task_id BIGINT NOT NULL,
-    user_id BIGINT,
+    author_id BIGINT,
 
-    CONSTRAINT fk_comment_task FOREIGN KEY (task_id)
-        REFERENCES public.tasks(id) ON DELETE CASCADE,
-    CONSTRAINT fk_comment_user FOREIGN KEY (user_id)
-        REFERENCES public.users(id) ON DELETE SET NULL
+    CONSTRAINT fk_comment_task FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
+    CONSTRAINT fk_comment_user FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE SET NULL
 );
