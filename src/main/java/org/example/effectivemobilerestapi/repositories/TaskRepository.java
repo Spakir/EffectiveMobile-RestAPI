37a73e5f8abe5fb65064fcde058d9703b87f789b
Spakir <danntyl@mail.ru>
2025-03-28 23:33:12 +0300

    Добавление join fetch и EntityGraph для устранения проблем N + 1

--- src/main/java/org/example/effectivemobilerestapi/repositories/TaskRepository.java
+++ src/main/java/org/example/effectivemobilerestapi/repositories/TaskRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -18,5 +20,6 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     @EntityGraph(attributePaths = {"comments", "author", "executor"})
     Page<Task> findByExecutorEmail(String executor, Pageable pageable);
 
-    Optional<Task> findById(Long id);
+    @EntityGraph(attributePaths = {"comments", "author", "executor"})
+    Optional<Task> findById(@Param("id") Long id);
 }
--- src/main/java/org/example/effectivemobilerestapi/repositories/UserRepository.java
+++ src/main/java/org/example/effectivemobilerestapi/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package org.example.effectivemobilerestapi.repositories;
 
 import org.example.effectivemobilerestapi.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,7 +11,10 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @EntityGraph(attributePaths = {"createdTasks", "assignedTasks"})
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.createdTasks LEFT JOIN FETCH u.assignedTasks WHERE u.email = :email")
     boolean existsByEmail(String email);
 
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = :email")
     Optional<User> findByEmail(String email);
 }
