9db6f22fadbc75204379e262b7d7ef1d2b0b1a0b
Spakir <danntyl@mail.ru>
2025-03-28 21:10:13 +0300

    Добавление обработки MethodArgumentNotValidException

--- src/main/java/org/example/effectivemobilerestapi/handlersExceptions/RestHandlerException.java
+++ src/main/java/org/example/effectivemobilerestapi/handlersExceptions/RestHandlerException.java
@@ -7,6 +7,8 @@ import org.example.effectivemobilerestapi.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -41,5 +43,22 @@ public class RestHandlerException {
     public ResponseEntity<ErrorResponseDto> handleGenericException(Exception ex) {
         ErrorResponseDto response = new ErrorResponseDto(ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleValidationException(MethodArgumentNotValidException ex){
+        String errorMessage = ex.getBindingResult().getAllErrors().stream()
+                .findFirst()
+                .map(error -> {
+                    if (error instanceof FieldError) {
+                        return String.format("%s", error.getDefaultMessage());
+                    }
+                    return error.getDefaultMessage();
+                })
+                .orElse("Неизвестная ошибка валидации");
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDto(errorMessage));
     }
 }
