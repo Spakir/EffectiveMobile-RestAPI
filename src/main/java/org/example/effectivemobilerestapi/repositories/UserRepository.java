4c0d7949c60a4baee5648db418c7362e5ff9840a
Spakir <danntyl@mail.ru>
2025-03-28 23:37:57 +0300

    Удаление @Query для методов UserRepository

--- src/main/java/org/example/effectivemobilerestapi/repositories/UserRepository.java
+++ src/main/java/org/example/effectivemobilerestapi/repositories/UserRepository.java
@@ -12,9 +12,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @EntityGraph(attributePaths = {"createdTasks", "assignedTasks"})
-    @Query("SELECT u FROM User u LEFT JOIN FETCH u.createdTasks LEFT JOIN FETCH u.assignedTasks WHERE u.email = :email")
     boolean existsByEmail(String email);
 
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = :email")
     Optional<User> findByEmail(String email);
 }
